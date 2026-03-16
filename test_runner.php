cc3634e01fef6275132bcc47e19d984da5ca3ef9
Dominik Możdżeń <dominikmozdzen@gmail.com>
2026-03-16 08:42:39 +0100

    hotfix

--- test_runner.php
+++ test_runner.php
@@ -1,24 +1,24 @@
 <?php
 header('Content-Type: application/json; charset=utf-8');
 
-$i = intval($_GET['i'] ?? 0);
+$index = intval($_GET['i'] ?? 0);
 $src = preg_replace('/[^a-zA-Z0-9_]/', '', $_GET['src'] ?? '');
 
 $base = __DIR__ . "/tematy/$src";
 
-$dane = "$base/dane/zadanie{$i}.php";
+$dane = "$base/dane/zadanie{$index}.php";
 if (file_exists($dane)) {
     include $dane;
 }
 
 ob_start();
-$rozw = "$base/rozwiazania/zadanie{$i}.php";
+$rozw = "$base/rozwiazania/zadanie{$index}.php";
 if (file_exists($rozw)) {
     include $rozw;
 }
 $output = ob_get_clean();
 
-$test = "$base/testy/zadanie{$i}.php";
+$test = "$base/testy/zadanie{$index}.php";
 if (!file_exists($test)) {
     echo json_encode([
         'poprawne' => false,
@@ -36,7 +36,6 @@ $wynik = [
     'komunikaty' => $komunikaty,
     'output' => $output
 ];
-
 
 echo json_encode($wynik, JSON_UNESCAPED_UNICODE);
 ?>
