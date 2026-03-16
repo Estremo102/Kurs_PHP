22947ea1051557efab965b57a0d14e7cae8cb2b2
Dominik Możdżeń <dominikmozdzen@gmail.com>
2026-03-16 08:56:51 +0100

    zadanie1 test hotfix

--- tematy/Petle/testy/zadanie1.php
+++ tematy/Petle/testy/zadanie1.php
@@ -2,9 +2,9 @@
 $out = str_replace(["<br>", "<br/>", "<br />"], "\n", $output);
 $out = str_replace(["\r\n", "\r"], "\n", $out);
 
-$linie = array_filter(array_map('trim', explode("\n", $out)));
+$linie = explode("\n", $out);
 
-$oczekiwane = ["0","10","20","30","40","50","60","70","80","90"];
+$oczekiwane = ["0","10","20","30","40","50","60","70","80","90",""];
 
 if (empty($linie)) {
     $komunikaty[] = "Nie wypisano danych, upewnij się, że korzystasz z echo";
