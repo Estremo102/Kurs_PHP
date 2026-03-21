b38a2d3c14c67912854d345c5b00cd07bff6bf47
Estremo102 <56071387+Estremo102@users.noreply.github.com>
2026-03-21 21:01:36 +0100

    temat funkcje zadanie 2

--- tematy/Funkcje/testy/zadanie2.php
+++ tematy/Funkcje/testy/zadanie2.php
@@ -1,3 +1,68 @@
 <?php
-$komunikaty[] = "nie zaimplementowano testów";
+if (!function_exists('pierwiastek')) {
+	$komunikaty[] = "Nie utworzono funkcji pierwiastek";
+	$komunikaty[] = "Zadanie niewykonane";
+	return;
+}
+
+$czyPoprawnaSygnatura = true;
+$ref = new ReflectionFunction('pierwiastek');
+$parametry = $ref->getParameters();
+
+if (count($parametry) !== 2) {
+	$czyPoprawnaSygnatura = false;
+	$komunikaty[] = "Funkcja pierwiastek powinna przyjmować dokładnie 2 argumenty";
+}
+
+if (isset($parametry[1])) {
+	if (!$parametry[1]->isDefaultValueAvailable() || $parametry[1]->getDefaultValue() != 2) {
+		$czyPoprawnaSygnatura = false;
+		$komunikaty[] = "Drugi argument funkcji pierwiastek powinien mieć wartość domyślną równą 2";
+	}
+} else {
+	$czyPoprawnaSygnatura = false;
+}
+
+$czyWynikiPoprawne = true;
+$testy = [
+	[9, 2, 3],
+	[27, 3, 3],
+	[16, 4, 2],
+	[81, 2, 9],
+];
+
+foreach ($testy as [$liczba, $stopien, $oczekiwane]) {
+	try {
+		$otrzymane = pierwiastek($liczba, $stopien);
+	} catch (Throwable $e) {
+		$czyWynikiPoprawne = false;
+		$komunikaty[] = "Błąd podczas wywołania pierwiastek($liczba, $stopien): " . $e->getMessage();
+		continue;
+	}
+
+	if (abs($otrzymane - $oczekiwane) > 0.000001) {
+		$czyWynikiPoprawne = false;
+		$komunikaty[] = "Błędny wynik dla pierwiastek($liczba, $stopien). Oczekiwano: $oczekiwane, Otrzymano: $otrzymane";
+	}
+}
+
+try {
+	$otrzymaneDomyslnie = pierwiastek(25);
+	if (abs($otrzymaneDomyslnie - 5) > 0.000001) {
+		$czyWynikiPoprawne = false;
+		$komunikaty[] = "Błędny wynik dla pierwiastek(25). Oczekiwano: 5, Otrzymano: $otrzymaneDomyslnie";
+	}
+} catch (Throwable $e) {
+	$czyWynikiPoprawne = false;
+	$komunikaty[] = "Nie można wywołać funkcji pierwiastek z jednym argumentem (domyślny stopień): " . $e->getMessage();
+}
+
+if ($czyPoprawnaSygnatura && $czyWynikiPoprawne) {
+	$poprawne = true;
+	$komunikaty[] = "Funkcja pierwiastek działa poprawnie i ma prawidłową wartość domyślną argumentu";
+	$komunikaty[] = "Zadanie wykonane poprawnie";
+} else {
+	$komunikaty[] = "Funkcja pierwiastek nie spełnia wymagań zadania";
+	$komunikaty[] = "Zadanie niewykonane";
+}
 ?>
--- tematy/Funkcje/tresc/zadanie2_opis.php
+++ tematy/Funkcje/tresc/zadanie2_opis.php
@@ -1,3 +1,22 @@
-<h3>Opis zadania 2</h3>
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid pariatur porro dicta perferendis quibusdam soluta recusandae. Accusantium ducimus perspiciatis velit, vitae tempore ipsum, cum incidunt illum exercitationem fuga repellendus doloribus.</p>
-<pre><code class="language-php"></code></pre>
+<h3>Domyślna wartość parametru</h3>
+<p>Możemy parametrom funkcji przypisywać domyślne wartości, które zostaną przypisane gdy nie zostaną one podane podczas wywoływania funkcji. Przykład:</p>
+<pre><code class="language-php">&lt;?php
+function hello($name = "nieznajomy") {
+    echo "Witaj {$name}<br>";
+}
+
+hello();
+hello("Jan");
+?&gt;</code></pre>
+<pre><code><?php
+function hello($name = "nieznajomy") {
+    echo "Witaj {$name}<br>";
+}
+
+hello();
+hello("Jan");
+?></code></pre>
+<p>
+    Pamiętasz funkcję sqrt()? Jeśli nie, przypomnij sobie ją w temacie Funkcje matematyczne. Twoim zadaniem jest napisanie funkcji która pozwoli liczyć pierwiastki także innego stopnia niż pierwiastek kwadratowy. funkcja pierwiastek powinna przyjmować 2 argumenty, drugi powinien mieć wartość domyślną równą 2. <br>
+    Podpowiedź: pierwiastkowanie to potęgowanie przez odwrotność. Użyj operatora potęgowania
+</p>
