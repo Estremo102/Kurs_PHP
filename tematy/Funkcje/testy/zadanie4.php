e2d9b0db38398b8d7712f899e1badbee6d295da7
Estremo102 <56071387+Estremo102@users.noreply.github.com>
2026-03-22 12:17:00 +0100

    funkcje zadanie 4

--- tematy/Funkcje/testy/zadanie4.php
+++ tematy/Funkcje/testy/zadanie4.php
@@ -1,4 +1,60 @@
 <?php
-$poprawne = false;
-$komunikaty[] = "nie zaimplementowano testów";
+if (!function_exists('oczyscTekst')) {
+	$komunikaty[] = "Nie utworzono funkcji oczyscTekst";
+	$komunikaty[] = "Zadanie niewykonane";
+	return;
+}
+
+$czyPoprawnaSygnatura = true;
+
+try {
+	$ref = new ReflectionFunction('oczyscTekst');
+	$parametry = $ref->getParameters();
+
+	if (count($parametry) !== 1) {
+		$czyPoprawnaSygnatura = false;
+		$komunikaty[] = "Funkcja oczyscTekst powinna przyjmować dokładnie 1 argument";
+	} elseif (!$parametry[0]->isPassedByReference()) {
+		$czyPoprawnaSygnatura = false;
+		$komunikaty[] = "Argument funkcji oczyscTekst powinien być przekazywany przez referencję (&)";
+	}
+} catch (Throwable $e) {
+	$czyPoprawnaSygnatura = false;
+	$komunikaty[] = "Nie udało się sprawdzić sygnatury funkcji oczyscTekst: " . $e->getMessage();
+}
+
+$czyWynikiPoprawne = true;
+
+$testy = [
+	["   Ala   ma    Kota   ", "ala ma kota"],
+	["    TO   JEST   TEST    ", "to jest test"],
+	["bez zmian", "bez zmian"],
+	["      ", ""],
+];
+
+foreach ($testy as [$wejscie, $oczekiwane]) {
+	$tekst = $wejscie;
+
+	try {
+		oczyscTekst($tekst);
+	} catch (Throwable $e) {
+		$czyWynikiPoprawne = false;
+		$komunikaty[] = "Błąd podczas wywołania oczyscTekst dla '$wejscie': " . $e->getMessage();
+		continue;
+	}
+
+	if ($tekst !== $oczekiwane) {
+		$czyWynikiPoprawne = false;
+		$komunikaty[] = "Błędny wynik dla '$wejscie'. Oczekiwano: '$oczekiwane', Otrzymano: '$tekst'";
+	}
+}
+
+if ($czyPoprawnaSygnatura && $czyWynikiPoprawne) {
+	$poprawne = true;
+	$komunikaty[] = "Funkcja oczyscTekst poprawnie modyfikuje tekst i ma prawidłową sygnaturę";
+	$komunikaty[] = "Zadanie wykonane poprawnie";
+} else {
+	$komunikaty[] = "Funkcja oczyscTekst nie spełnia wymagań zadania";
+	$komunikaty[] = "Zadanie niewykonane";
+}
 ?>
--- tematy/Funkcje/tresc/zadanie4_opis.php
+++ tematy/Funkcje/tresc/zadanie4_opis.php
@@ -1,3 +1,54 @@
-<h3>Opis zadania 4</h3>
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid pariatur porro dicta perferendis quibusdam soluta recusandae. Accusantium ducimus perspiciatis velit, vitae tempore ipsum, cum incidunt illum exercitationem fuga repellendus doloribus.</p>
-<pre><code class="language-php"></code></pre>
+<h3>Przekazywanie argumentów przez odniesienie</h3>
+<p>Argumenty są zazwyczaj przekazywane przez wartość, co oznacza, że w funkcji używana jest kopia wartości, a zmienna przekazana do funkcji nie może zostać zmieniona.</p>
+<p>Gdy argument funkcji jest przekazywany przez referencję, zmiany w argumencie zmieniają również przekazaną zmienną. Aby przekształcić argument funkcji w referencję, należy użyć operatora & przed argumentem/parametrem:</p>
+<pre><code class="language-php">&lt;?php
+function dodajDwa1($liczba) {
+    $liczba += 2;
+}
+
+function dodajDwa2(&$liczba) {
+    $liczba += 2;
+}
+
+$x = 2;
+echo "$x&lt;br>";
+dodajDwa1($x);
+echo "$x&lt;br>";
+dodajDwa2($x);
+echo "$x&lt;br>";
+?></code></pre>
+<p>Rezultat:</p>
+<pre><code><?php
+function dodajDwa1($liczba) {
+    $liczba += 2;
+}
+
+function dodajDwa2(&$liczba) {
+    $liczba += 2;
+}
+
+$x = 2;
+echo "$x<br>";
+dodajDwa1($x);
+echo "$x<br>";
+dodajDwa2($x);
+echo "$x<br>";
+?></code></pre>
+<p>Jak możesz zauważyć, brak znaku & sprawia, że liczba w funkcji to kopia, jednak jeśli użyjemy tego znaku działamy na oryginalnej zmiennej.</p>
+<p>
+    Twoim zadaniem jest napisanie funkcji oczyscTekst, która przyjmuje jeden parametr i modyfikuje go w następujący sposób: usuwa spacje z początku i końca tekstu, zamienia wiele spacji na jedną, zamienia cały tekst na małe litery. <br>
+    Przykładowe rozwiązania: 
+    <ol>
+        <li>trim + preg_replace (wymaga regex, który dopiero będziemy poznawać w przyszłych lekcjach) + strtolower</li>
+        <li>trim + explode + array_filter + implode + strtolower</li>
+    </ol>
+    co robią powyższe funkcje?
+    <ul>
+        <li>trim usuwa spacje z początku i końca.</li>
+        <li>explode rozbija tekst na fragmenty.</li>
+        <li>array_filter usuwa te puste elementy.</li>
+        <li>skleja elementy tablicy w jednego stringa.</li>
+        <li>normalizuje wielkość liter.</li>
+        <li>preg_replace można powiedzieć, że to bardziej rozbudowana wersja str_replace.</li>
+    </ul>
+</p>
