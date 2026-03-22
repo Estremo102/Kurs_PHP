a1da2d04ab098030f27df87e41a8a8e7b01b96b7
Estremo102 <56071387+Estremo102@users.noreply.github.com>
2026-03-22 13:07:11 +0100

    funkcje zadanie 5 tresc

--- tematy/Funkcje/tresc/zadanie5_opis.php
+++ tematy/Funkcje/tresc/zadanie5_opis.php
@@ -1,3 +1,61 @@
-<h3>Opis zadania 5</h3>
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid pariatur porro dicta perferendis quibusdam soluta recusandae. Accusantium ducimus perspiciatis velit, vitae tempore ipsum, cum incidunt illum exercitationem fuga repellendus doloribus.</p>
-<pre><code class="language-php"></code></pre>
+<h3>Zmienna liczba parametrów</h3>
+<p>Używając operatora ... przed parametrem funkcji, funkcja akceptuje nieznaną liczbę parametrów. Nazywa się to również funkcją o zmiennej liczbie parametrów.</p>
+<p>Argument funkcji o zmiennej liczbie parametrów staje się tablicą. Przykład:</p>
+<pre><code class="language-php">&lt;?php
+function sumMyNumbers(...$x) {
+  $n = 0;
+  $len = count($x);
+  for($i = 0; $i < $len; $i++) {
+    $n += $x[$i];
+  }
+  return $n;
+}
+
+$a = sumMyNumbers(5, 2, 6, 2, 7, 7);
+echo $a;
+?></code></pre>
+<p>Rezultat:</p>
+<pre><code><?php
+function sumMyNumbers(...$x) {
+  $n = 0;
+  $len = count($x);
+  for($i = 0; $i < $len; $i++) {
+    $n += $x[$i];
+  }
+  return $n;
+}
+
+$a = sumMyNumbers(5, 2, 6, 2, 7, 7);
+echo $a;
+?></code></pre>
+<p>Powyższy kod przyjmuje nieokreśloną ilość parametrówm a następnie wszystkie ze sobą sumuje i zwraca wynik</p>
+<p>Funkcja może przymować jednocześnie zwykłe parametry i ich nieokreśloną ilość. Trzeba jednak pamiętać, że operatora ... można użyć tylko przy ostatnim argumencie. Przykład:</p>
+<pre><code class="language-php">&lt;?php
+function myFamily($lastname, ...$firstname) {
+  $txt = "";
+  $len = count($firstname);
+  for($i = 0; $i < $len; $i++) {
+    $txt = $txt."Witaj, $firstname[$i] $lastname.<br>";
+  }
+  return $txt;
+}
+
+$a = myFamily("Nowak", "Jan", "Anna", "Michał");
+echo $a;
+?></code></pre>
+<p>Rezultat:</p>
+<pre><code><?php
+function myFamily($lastname, ...$firstname) {
+  $txt = "";
+  $len = count($firstname);
+  for($i = 0; $i < $len; $i++) {
+    $txt = $txt."Witaj, $firstname[$i] $lastname.<br>";
+  }
+  return $txt;
+}
+
+$a = myFamily("Nowak", "Jan", "Anna", "Michał");
+echo $a;
+?></code></pre>
+<p>Więc gdyby napisać <pre><code>function myFamily(...$firstname, $lastname) {</code></pre> to wystąpiłby błąd.</p>
+<p>Pamiętasz funkcje min() i max()? Jeśli nie możesz zaglądnąć do tematu Funkcje matematyczne. Twoim zadaniem jest zaimplementowanie własnych funkcji myMin oraz myMax które będą działać identycznie.</p>
