1aa8d370b0a211288e5aaee52e079a1faefc318c
Estremo102 <56071387+Estremo102@users.noreply.github.com>
2026-04-19 20:42:05 +0200

    tablice zadanie 1 treść

--- tematy/Tablice/tresc/zadanie1_opis.php
+++ tematy/Tablice/tresc/zadanie1_opis.php
@@ -1,3 +1,14 @@
-<h3>Opis zadania 1</h3>
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid pariatur porro dicta perferendis quibusdam soluta recusandae. Accusantium ducimus perspiciatis velit, vitae tempore ipsum, cum incidunt illum exercitationem fuga repellendus doloribus.</p>
-<pre><code class="language-php"></code></pre>
+<h3>Tablice - wprowadzenie</h3>
+<p>Tablice to zmienne które mogą przechowywać wiele wartości.</p>
+<pre><code class="language-php">$cars = array("Volvo", "BMW", "Toyota");</code></pre>
+<p>Powyższy kod tworzy tablicę z 3 samochodami.</p>
+<p>W php możemy wyróżnić 3 rodzaje tablic:
+    <ul>
+        <li>tablice indeksowane - do kolejnych wartości odwołujemy się poprzez numer indeksu</li>
+        <li>tablice asocjacyjne - do wartości odwołujemy się po nazwie klucza</li>
+        <li>tablice wielowymiarowe - tablice zawierające w sobie tablice jako wartości</li>
+    </ul>
+</p>
+<p>Wartości w tablicy mogą być dowolne, najczęściej są to liczby, ciągi znaków i inne proste zmienne, ale można przechowywać także np. obiekty czy funkcje</p>
+<p>Funkcja count() zwraca ilość elementów w tablicy.</p>
+<p>Zadanie: W kodzie masz dostęp do zmiennej tablica, wyświetl jej długość</p>
