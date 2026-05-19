4edab758ba8d01e7b33e6dac1914cfb4cf267fa1
Dominik Możdżeń <dominikmozdzen@gmail.com>
2026-05-19 14:32:21 +0200

    $_REQUEST

--- tematy/Zmienne_superglobalne/tresc/zadanie4_opis.php
+++ tematy/Zmienne_superglobalne/tresc/zadanie4_opis.php
@@ -1,3 +1,6 @@
-<h3>Opis zadania 4</h3>
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid pariatur porro dicta perferendis quibusdam soluta recusandae. Accusantium ducimus perspiciatis velit, vitae tempore ipsum, cum incidunt illum exercitationem fuga repellendus doloribus.</p>
+<h3>$_COOKIE</h3>
+<h4>$_REQUEST</h3>
+<p>zmienna $_REQUEST zawiera dane z przesyłanych formularzy, ciągi zapytań URL i pliki cookie HTTP, czyli jest to tablica zawierająca dane z $_GET, $_POST oraz $_COOKIE. Jest to wygodna opcja, jednak może stwarzać luki w zabezpieczeniach, dlatego lepiej używać $_COOKIE, $_POST i $_GET</p>
+<h4>$_COOKIE</h4>
+<p></p>
 <pre><code class="language-php"></code></pre>
