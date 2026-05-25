109574525b34014c054997750a8cfc5a0f7e8de4
Dominik Możdżeń <dominikmozdzen@gmail.com>
2026-05-25 08:45:30 +0200

    wzorce regex

--- tematy/RegEx/tresc/zadanie1_opis.php
+++ tematy/RegEx/tresc/zadanie1_opis.php
@@ -2,17 +2,17 @@
 <h4>Czym jest RegEx?</h4>
 <p>RegEx (Regular Expressions) czyli Wyrażenia regularne, pozwala ono na tworzenie wzorców, na podstawie których można wyszukiwać w tekście konkretnych schematów. Przykładowo można wyszukać lub zasąpić tekst.</p>
 <h4>Składnia</h4>
-<p>w PHP RegEx to ciągi złożone z ograniczników, wzór i opcjonalne modyfikatory.</p>
-<pre><code class="language-php">"/wzor/modyfikatory"</code></pre>
+<p>w PHP RegEx to ciągi złożone z ograniczników, wzorzec i opcjonalne modyfikatory.</p>
+<pre><code class="language-php">"/wzorzec/modyfikatory"</code></pre>
 <p>
     <ul>
         <li>ograniczniki - znaki które zamyjaką wzór (np. <code>/</code>)</li>
-        <li>wzór - sekwencja znaków do wyszukania</li>
+        <li>wzorzec - sekwencja znaków do wyszukania</li>
         <li>modyfikatory - sposób wykonania wyszukiwania</li>
     </ul>
     Przykład:
     <pre><code class="language-php">$wyr = "/zsht/i";</code></pre>
-    Tutaj <code>/</code> jest ogranicznikiem, <code>zsht</code> jest wzorem, a <code>i</code> modyfikatorem. <br>
-    ogranicznik może byćdowolnym znakiem który nie jest liczbą, literą backslashashem lub spacją. Najczęstrzym ograncznikiem jest slash, ale gdy potrzeba go użyć we wzorze, wtedy można użyć innych znaków takich jak # lub ~.
+    Tutaj <code>/</code> jest ogranicznikiem, <code>zsht</code> jest wzorcem, a <code>i</code> modyfikatorem. <br>
+    ogranicznik może byćdowolnym znakiem który nie jest liczbą, literą backslashashem lub spacją. Najczęstrzym ograncznikiem jest slash, ale gdy potrzeba go użyć we wzorcu, wtedy można użyć innych znaków takich jak # lub ~.
 </p>
-<p>Zadanie: todo zamiana / na ~ lub # w gotowym wyrażeniu regularnym zawierającym slash</p>
+<p>Zadanie: todo zamiana ograniczników / na ~ lub # w gotowym wyrażeniu regularnym zawierającym slash.</p>
--- tematy/RegEx/tresc/zadanie2_opis.php
+++ tematy/RegEx/tresc/zadanie2_opis.php
@@ -1,3 +1,60 @@
-<h3>Opis zadania 2</h3>
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid pariatur porro dicta perferendis quibusdam soluta recusandae. Accusantium ducimus perspiciatis velit, vitae tempore ipsum, cum incidunt illum exercitationem fuga repellendus doloribus.</p>
+<h3>Modyfikatory i wzorce RegEx</h3>
+<h4>Modyfikatory</h4>
+<table>
+    <tr>
+        <th>Modyfikator</th>
+        <th>Opis</th>
+    </tr>
+    <tr>
+        <td>i</td>
+        <td>nie uwzględnia wielkości liter</td>
+    </tr>
+    <tr>
+        <td>m</td>
+        <td>wyszukiwania wielowierszowe</td>
+    </tr>
+    <tr>
+        <td>u</td>
+        <td>umożliwia poprawne dopasowanie wzorców zakodowanych w UTF-8</td>
+    </tr>
+</table>
+<h4>Wzorce</h4>
+<table>
+    <tr>
+        <th>Wyrażenie</th>
+        <th>Opis</th>
+    </tr>
+    <tr>
+        <td>[abc]</td>
+        <td>znajduje jeden lub wiele znaków znajdujących się w nawiasach</td>
+    </tr>
+    <tr>
+        <td>[^abc]</td>
+        <td>znajduje dowolny znak nie znajdujący się między nawiasami</td>
+    </tr>
+    <tr>
+        <td>[a-z]</td>
+        <td></td>
+    </tr>
+    <tr>
+        <td>[A-z]</td>
+        <td></td>
+    </tr>
+    <tr>
+        <td>[A-Z]</td>
+        <td></td>
+    </tr>
+    <tr>
+        <td>[123]</td>
+        <td></td>
+    </tr>
+    <tr>
+        <td>[0-5]</td>
+        <td></td>
+    </tr>
+    <tr>
+        <td>[0-9]</td>
+        <td></td>
+    </tr>
+</table>
 <pre><code class="language-php"></code></pre>
