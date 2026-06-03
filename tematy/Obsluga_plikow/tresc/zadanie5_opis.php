9561db1f4ce6dc08592ea1cf551fdd9bf76ad4f4
Dominik Możdżeń <dominikmozdzen@gmail.com>
2026-06-03 11:08:33 +0200

    zadanie 5

--- tematy/Obsluga_plikow/tresc/zadanie5_opis.php
+++ tematy/Obsluga_plikow/tresc/zadanie5_opis.php
@@ -1,3 +1,7 @@
 <h3>Utworzenie pliku</h3>
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid pariatur porro dicta perferendis quibusdam soluta recusandae. Accusantium ducimus perspiciatis velit, vitae tempore ipsum, cum incidunt illum exercitationem fuga repellendus doloribus.</p>
-<pre><code class="language-php"></code></pre>
+<p>Funkcja fopen() służy także do tworzenia plików. Jeśli w trybie <code>w</code> lub <code>a</code> spróbujemy otworzyć plik, który nie istnieje, zostanie on utworzony.</p>
+<pre><code class="language-php">$myfile = fopen('tematy\Obsluga_plikow\\'. date("Y.m.d_His").".txt", "w");</code></pre>
+<?php $myfile = fopen('tematy\Obsluga_plikow\\'. date("Y.m.d_His").".txt", "w"); ?>
+<p>Powyższy kod tworzy podczas otwarcia strony plik z nazwą wskazującą datę i godzinę jej otwarcia.</p>
+<p>Zadanie: Napisz kod, który w folderze rozwiazania utworzy plik txt z nazwą zwróconą przez funkcję <code>get_current_user();</code>.</p>
+<?php $myfile = fopen('tematy\Obsluga_plikow\rozwiazania\\'. get_current_user().".txt", "w"); ?>
